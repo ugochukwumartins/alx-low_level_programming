648de299ef570f4138ecfbb02e4a4321f94e4104
ugochukwumartins <uokorocha72@gmail.com>
2023-03-17 22:33:12 -0700

    added stuff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 48;
+int n2;
+int c = 44;
+while (n1 <= 57)
+{
+	n2 = n1 + 1;
+while (n2 <= 57)
+{
+	putchar(n1);
+	putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+	putchar(c);
+	putchar(32);
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
+}
