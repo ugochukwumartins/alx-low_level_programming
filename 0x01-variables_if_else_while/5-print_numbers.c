225cb712a2e36baab793cbb4e3852fc8f8061974
ugochukwumartins <uokorocha72@gmail.com>
2023-03-17 22:12:05 -0700

    added stuff

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main -Entry point
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+	printf("%d", n);
+	n++;
+}
+putchar('\n');
+return (0);
+}
