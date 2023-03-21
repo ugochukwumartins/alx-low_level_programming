6f3f7103a99285ed91b86ddf8019cea2013dd912
ugochukwumartins <uokorocha72@gmail.com>
2023-03-21 08:46:54 -0700

    value added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add -> adds two intergers
+ * @i: parameter 1
+ * @j: parameter 2
+ * Return: Returns the result
+ */
+int add(int i, int j)
+{
+	int res;
+
+	res = (i + j);
+	return (res);
+}
