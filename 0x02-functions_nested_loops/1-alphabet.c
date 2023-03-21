a66ec72b06b40be8e1705e99d6895e5326bd46c6
ugochukwumartins <uokorocha72@gmail.com>
2023-03-21 07:58:50 -0700

    addedvalue

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ *
+ */
+void print_alphabet(void)
+{
+int j = 'a';
+while (j <= 'z')
+{
+	_putchar(j);
+	j++;
+}
+_putchar('\n');
+}
