a84872c18f03bb6501045001d28af24078c9f3ca
ugochukwumartins <uokorocha72@gmail.com>
2023-04-20 15:56:50 -0700

    added stuff

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,47 +12,45 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i= 0, j, c=0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
 	va_star(valist, format);
 	while (format && format[i])
 	{
-		j=0;
+		j = 0;
 		while (t_arg[j])
 		{
 		       	if (format[i] == t_arg[j] && c)
 			{
-			printf(", ");
-		break;
+				printf(", ");
+				break;
 			} j++;
 		}
-	switch (format[i])
-	{
-		 case 'c' :
-		         printf("%c", var_arg(valist, int)), c=1;
-			 break;
-		 case 'i':
-                         printf("%d", var_arg(valist, int)), c=1;
-                         break;
-		 case 'f':
-                         printf("%f", var_arg(valist, double)), c=1;
-                         break;
-		 case 's':
-                         str =  var_arg(valist, char *), c=1;
-                         if (!str)
-			 {
-				  
-                         printf("(nil)");
-                         break;
+		switch (format[i])
+		{
+	 	case 'c':
+			 printf("%c", var_arg(valist, int)), c = 1;
+		 	break;
+		case 'i':
+        	         printf("%d", var_arg(valist, int)), c = 1;
+                	 break;
+		case 'f':
+        	         printf("%f", var_arg(valist, double)), c = 1;
+                	 break;
+		case 's':
+        	        str =  var_arg(valist, char *), c = 1;
+               		 if (!str)
+			 {  
+                	        printf("(nil)");
+                        	break;
 			 }
-                         printf("%s",str);
-                         break;
-	} i++;
+               		 printf("%s",str);
+              		 break;
+	  	  } i++;
 	}
-	 case 'c' :
-                         printf("\n"), va_end(valist);
+	printf("\n"), va_end(valist);
 }
 
 
