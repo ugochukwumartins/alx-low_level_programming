e4b0e953144799ee26d872a08bd1af98dc117e35
ugochukwumartins <uokorocha72@gmail.com>
2023-04-20 16:03:21 -0700

    added stuff

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,51 @@
-#include "variadic_function.h"
-#include  <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
- * print_all - print anything.
- * @format: a listof type of argument passed to the function.
- *
- * Return : no return .
- */
-
+  *print_all - prints anything.
+  *@format: list of all arguments passed to the function.
+  *
+  *Return: void.
+  */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	unsigned int i;
+	va_list args;
+	char *s, *separator;
 
-	va_star(valist, format);
+	va_start(args, format);
+
+	separator = "";
+
+	i = 0;
 	while (format && format[i])
 	{
-		j = 0;
-		while (t_arg[j])
-		{
-		       	if (format[i] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
 		switch (format[i])
 		{
-	 	case 'c':
-			 printf("%c", var_arg(valist, int)), c = 1;
-		 	break;
-		case 'i':
-        	         printf("%d", var_arg(valist, int)), c = 1;
-                	 break;
-		case 'f':
-        	         printf("%f", var_arg(valist, double)), c = 1;
-                	 break;
-		case 's':
-        	        str =  var_arg(valist, char *), c = 1;
-               		 if (!str)
-			 {  
-                	        printf("(nil)");
-                        	break;
-			 }
-               		 printf("%s",str);
-              		 break;
-	  	  } i++;
+			case 'c':
+				printf("%s%c", separator,  va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
 	}
-	printf("\n"), va_end(valist);
+
+	printf("\n");
+	va_end(args);
 }
-
-
