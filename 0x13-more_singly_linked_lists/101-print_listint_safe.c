5f0013f71ae938c8a8b763c499450e71bb135aa6
ugochukwumartins <uokorocha72@gmail.com>
2023-05-02 05:41:00 -0700

    completed the advance task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint_safe - prints list, avoids loop
+ * @head: points to start of list
+ * Return: number of nodes in size_t or exit 98
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodeCount = 0;
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		nodeCount += 1;
+
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (nodeCount);
+}
